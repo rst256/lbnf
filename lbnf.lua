329da20c3071281b8fc90c453b25e63741578f48
rst256 <ussr24@yandex.ru>
2016-03-22 21:02:18 +0700

    Before: change send captured info from arg ctx to return value

--- lbnf.lua
+++ lbnf.lua
@@ -1,20 +1,16 @@
 
 local M = {}
 
+
+
+
+
+
 local function indexof(parent, predefs)
 	return setmetatable(predefs or {}, { __index=parent })
 end
 
 
--- local function push_scope(parent, predefs)
--- 		local c2={}
--- 		local idx, out = rule(s, start, indexof(ctx, {capture=c2}))
--- 		local idx, out = rule(s, start, (ctx ))
--- 		-- if idx then table.insert(ctx.capture, c2) print(idx, table.unpack(c2)) end
--- 		return idx, out-- or c2
--- 
--- 	return setmetatable(predefs or {}, { __index=parent })
--- end
 
 
 local lbnfoper_mt = {}
@@ -23,27 +19,25 @@ local lbnfoper_funcs = {}
 
 
  -- named_capture
-function lbnfoper_funcs:alt(source, start, ctx)
+function lbnfoper_funcs:combineRules(source, start, ctx)
 	local idx, captopt = start, rawget(self, 'capture_options')
 	for _, rule in ipairs(self.rules) do
 		local c2={}
 		local idx, out = rule(source, start, indexof(ctx, {capture=c2}))
 		if idx then
-			if #c2==1 then table.insert(ctx.capture, c2[1]) else
-				table.insert(ctx.capture, c2)  end
+			--if #c2==1 then table.insert(ctx.capture, c2[1]) else
+				table.insert(ctx.capture, c2)  --end
 
-			-- if captopt then ctx.capture[captopt] = out else table.insert(ctx.capture, out)  end
 			return idx, out
 		end
 	end
 end
 
-function lbnfoper_funcs:rep(source, start, ctx)
+function lbnfoper_funcs:repeatRule(source, start, ctx)
 	local idx= start
 	::loop::
 		local i, c = self.rule(source, idx, ctx)
 		if not i then return idx, c end assert(i~=idx)
-
 		idx = i;
 	goto loop
 end
@@ -57,37 +51,62 @@ function lbnfoper_funcs:seq(source, start, ctx)
 	return idx, out
 end
 
-function lbnfoper_funcs:l(source, start, ctx)
-			local capname = rawget(self, 'capture_name')
-	local out, idx = source:match('^%s*'..self.pattern..'()', start)
-		if idx then
-			if capname then ctx.capture[capname]=out else 	table.insert(ctx.capture, out)  end
-		end
+-- function lbnfoper_funcs:listRule(source, start, ctx)
+-- 	local idx= start
+-- 	::loop::
+-- 		local i, c = (self.first or self.items)(source, idx, ctx)
+-- 		if not i then return end idx = i 
+-- 
+-- 	goto loop
+-- end
+-- 
+-- function M.listRule(list)
+-- 	list.parse=lbnfoper_funcs.listRule
+-- 	return setmetatable(list, lbnfoper_mt)
+-- end
 
-		if idx==nil then return out elseif out=='' then return idx end
-		return idx, out
+function lbnfoper_funcs:ultraFilter(source, start, ctx)
+	local capname = rawget(self, 'capture_name')
+	local idx, out = self.filter_func(source, start, self.filter_opts)
+	if idx then
+		if capname then ctx.capture[capname]=out else 	table.insert(ctx.capture, out)  end
+	end
+	return idx
 end
 
 
-
-function lbnfoper_mt:__index(name)
-	if lbnfoper_attribs[name] then rawset(self, name, true) end
-	return self
-end
-
-
+-- function lbnfoper_mt:__index(name)
+-- 	if lbnfoper_attribs[name] then rawset(self, name, true) end
+-- 	return self
+-- end
+-- 
+-- 
 
 
 
 function lbnfoper_mt:__call(source, start, ctx)
-	-- if self.par
+	-- if self.parse~=lbnfoper_funcs.ultraFilter then
+	-- 	local c2={}
+	-- 	local idx, out = rule(source, start, indexof(ctx, {capture=c2}))
+	-- 	if idx then
+	-- 		if #c2==1 then table.insert(ctx.capture, c2[1]) else table.insert(ctx.capture, c2)  end
+	-- 		return idx, out
+	-- 	end
+	-- else
+		local idx, out = self:parse(source, start, ctx)	
+		if idx~=nil and self.handler then 
+			local result = self:handler(ctx, out)
+			if result==false then return elseif result==nil then return idx, out else return idx, result end
+		else
+			return idx, out
+		end
 		-- local c2={}
 		-- local idx, out = rule(source, start, indexof(ctx, {capture=c2}))
 		-- if idx then
 			-- if #c2==1 then table.insert(ctx.capture, c2[1]) else
 				-- table.insert(ctx.capture, c2)  end
 
-	return self:parse(source, start, ctx)
+
 end
 
 
@@ -113,20 +132,26 @@ end
 
 
 function M.combineRules(...)
-	return setmetatable({ parse=lbnfoper_funcs.alt, rules={...} }, lbnfoper_mt)
+	return setmetatable({ parse=lbnfoper_funcs.combineRules, rules={...} }, lbnfoper_mt)
 end
 
 function M.repeatRule(repeated_rule)
-	return setmetatable({ parse=lbnfoper_funcs.rep, rule=repeated_rule }, lbnfoper_mt)
+	return setmetatable({ parse=lbnfoper_funcs.repeatRule, rule=repeated_rule }, lbnfoper_mt)
 end
 
 function M.sequenceRules(...)
 	return setmetatable({ parse=lbnfoper_funcs.seq, seq_rules={...} }, lbnfoper_mt)
 end
 
+
+
 function M.stringPattern(pattern)
-	return setmetatable({ parse=lbnfoper_funcs.l, pattern=pattern }, lbnfoper_mt)
+	return M.ultraFilter(function(source, start, pattern)
+		local out, idx = source:match('^%s*'..pattern..'()', start)
+		if not idx then 	return out else return idx, out end
+	end, pattern)
 end
+
 
 
 function M.optionalRule(optional_rule)
@@ -136,13 +161,6 @@ function M.optionalRule(optional_rule)
 end
 
 
--- function l(pattern)
--- 	return function(s, start, ctx)
--- 		local out, idx = s:match('^%s*'..pattern..'()', start)
--- 		if idx==nil then return out elseif out=='' then return idx end
--- 		return idx, out
--- 	end
--- end
 
 function M.referenceRule(table_of_rules, rule_name)
 	return function(s, start, ctx)
@@ -155,5 +173,13 @@ function M.referenceRule(table_of_rules, rule_name)
 		return idx, out-- or c2
 	end
 end
+
+function M.ultraFilter(filter_func, filter_opts)
+	return setmetatable({ 
+		parse=lbnfoper_funcs.ultraFilter, filter_func=filter_func, filter_opts=filter_opts 
+	}, lbnfoper_mt)
+end
+
+
 
 return M
--- test-lbnf.lua
+++ test-lbnf.lua
@@ -12,49 +12,78 @@ local ref = lbnf.referenceRule
 
 
 
-
+function getRule(l)
 
 local char = l"('[^']+')"
 local str = l'(".-[^\\]")'
 local id = l'([_%w]+[_%w%d]*)'
 local num = l"(-?%d+%.?%d*)"
-
+-- id.handler=function(x, c, o) print(x, c, o) if o=='x322' then  return false end end
 local gmr1 = setmetatable({}, { __index=ref })
 
 local value = alt(
 	gmr1.fncall,
-	id, num, str, char, seq( l'%(', gmr1.expr, l'%)' )
-
+	id, num, str, char, seq( l'%(', gmr1.expr, l'%)' ) 
 )
-gmr1.expr = seq(
-	value
-		,rep(seq( alt( l'(%-)', l'(%+)', l'(%*)' ), value))
-)
-gmr1.fncall = seq( id^'func', l'%(', rep(seq( ref(gmr1,'expr'), l'(,?)' ))^'args' ,l'%)' )
+-- gmr1.expr = seq(
+-- 	value
+-- 		,rep(seq( alt( l'(%-)', l'(%+)', l'(%*)',seq(l'(%|)', l'(%|)') ), value))
+-- )
+gmr1.expr =seq(value, opt(seq(alt( l'(%-)', l'(%+)', l'(%*)' ), gmr1.expr)) )
+-- gmr1.expr =alt(
+-- 	seq(value, alt( l'(%-)', l'(%+)', l'(%*)' ), gmr1.expr),
+-- 	value
+-- )
+-- gmr1.expr=
+-- gmr1.expr_list =alt(
+-- 	seq( gmr1.expr, l',', gmr1.expr_list ),
+-- 	gmr1.expr
+-- )
+gmr1.expr_list = rep(seq( gmr1.expr, opt(l',') ))
+-- gmr1.expr_list = seq( gmr1.expr, opt(seq(l',', gmr1.expr_list)) )
+
+gmr1.fncall = seq( id, l'%(', seq( gmr1.expr,rep(seq( l',' ,gmr1.expr ))) ,l'%)' )
+-- gmr1.fncall = seq( id^'func', l'%(', rep(seq( gmr1.expr, l',?' ))^'args' ,l'%)' )
+-- gmr1.fncall.handler=print
+  return gmr1.expr
+end
 
 
-local src0 = '66+6 *063+(1+ x +34+6) +  func( x*6,4*sin(0))'
-local src = ' 1 + 2 * ( 31 - fn32  (  321 *  x322 , ( 4 * sin(0) ) )  )  + 0'
+
+local src0 = '66||6 *063+(1+ x +34+6) +  func( x*6,4*sin(0))'
+-- local src = ' 1 + 2 * f( 31  )  + 0'
+local src = ' 1 + 2 -3 * ( 31 - fn32  (  321 *  x322,( 4 * sin(0) ) )  )  + 0'
 
 print()
-
-ctx={ capture={} }
-local i, c = gmr1.expr(src, 1, ctx)
 --inspect(i, (c), #src)
 
 local function tostring_(ss)
 	local s_num, s_str = '', ''
 	for k, v in pairs(ss) do
-		if type(v)=='table' then s_num=s_num..' {'..tostring_(v) ..'} '
+		if type(v)=='table' then s_num=s_num..'{'..tostring_(v) ..'}'
 		elseif type(k)=='number' then s_num=s_num..' '..v
-		else s_str=s_str..' '..k..': '..v..', ' end
+		else s_str=s_str..k..':'..v..' ' end
 	end
-	return s_str..' '..s_num
+	return s_str..s_num
 end
 
 
-print(tostring_(ctx.capture):gsub('(%s+)', ' '))
-print(src:gsub('(%s+)', ' '))
+ctx={ capture={} }
+local i, c = getRule(l)(src, 1, ctx)
+out1=tostring_(ctx.capture):gsub('(%s+)', ' ')
+
+-- local l = lbnf.stringPattern
+
+-- ctx={ capture={} }
+-- local i, c = getRule(lbnf.stringPattern)(src, 1, ctx)
+-- out2=tostring_(ctx.capture):gsub('(%s+)', ' ')
+-- 
+-- print(out1)
+-- assert(out1==out2)
+
+
+print(tostring_(ctx.capture))
+print(src)
 -- inspect(i,  ctx.capture)
 
 
